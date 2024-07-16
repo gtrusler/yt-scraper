81d49133f9ea6e05d59606501d2a50f2a2c05b15
Graydon Trusler <gtrusler@gmail.com>
2024-07-16 12:05:46 -0500

    Initial commit

---
+++ yt-scraper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source "venv/bin/activate"
+
+# Run the Python script
+python3 /Users/gtrusler/Developer/yt-scraper/yt-scraper.py
+
+# Deactivate the virtual environment
+deactivate
